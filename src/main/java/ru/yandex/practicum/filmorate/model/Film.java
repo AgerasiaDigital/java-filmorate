b78b2646f1471108e9b4a90e4d8d7340916971df
Oleg Digital <boss@agerasiadigital.ru>
2025-08-02 15:14:43 +0300

    Исправлен для проверки на github

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import ru.yandex.practicum.filmorate.validation.Create;
 import ru.yandex.practicum.filmorate.validation.Update;
+import ru.yandex.practicum.filmorate.validation.ReleaseDateConstraint;
 
 import jakarta.validation.constraints.*;
 import java.time.LocalDate;
@@ -14,15 +15,16 @@ public class Film {
     @NotNull(groups = Update.class, message = "ID обязателен для обновления")
     private Long id;
 
-    @NotBlank(groups = Create.class, message = "Название не может быть пустым")
+    @NotBlank(groups = {Create.class}, message = "Название не может быть пустым")
     private String name;
 
-    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    @Size(max = 200, groups = {Create.class, Update.class}, message = "Максимальная длина описания — 200 символов")
     private String description;
 
+    @ReleaseDateConstraint(groups = {Create.class, Update.class})
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
 
-    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    @Positive(groups = {Create.class, Update.class}, message = "Продолжительность фильма должна быть положительным числом")
     private Integer duration;
 }
