e169551fa52f1a0f69deb8aac1c7682feb542231
Oleg Digital <boss@agerasiadigital.ru>
2025-09-04 12:43:57 +0300

    Изменения для проверки github

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface GenreStorage {
+
     Optional<Genre> findById(int id);
+
     Collection<Genre> findAll();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaStorage.java
@@ -6,8 +6,8 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface MpaStorage {
+
     Optional<Mpa> findById(int id);
 
     Collection<Mpa> findAll();
 }
-
